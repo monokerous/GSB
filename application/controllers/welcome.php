31d6a1fb16747efb971e7d8a46a3a7240d526ec0
BarataMathieu <mathieu@barata.fr>
2017-01-08 17:46:39 +0100

    no message

--- application/controllers/welcome.php
+++ application/controllers/welcome.php
@@ -1,15 +1,16 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-require('praticien.php');
+require('praticien.php'); //call class Praticien
 class Welcome extends CI_Controller {
 
-		//CONSTRUCTEUR PAR DEFAUT DE LA CLASSE Praticien
-
+	/**
+         * CONSTRUCTEUR PAR DEFAUT DE LA CLASSE Praticien
+         */
 	public function __construct(){
 		//on appel le constructeur de la classe m�re
 		parent:: __construct();
 	}
 	/**
-         * Cette méthode appelle la fonction du index du controller  index
+         * Cette méthode appelle la méthode du index du controller Praticien
          */
 	public function index() {
             $practicien  = new Praticien();
