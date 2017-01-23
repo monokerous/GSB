4ba92ba1584e4d87db5527186fc4240dc721e20c
BarataMathieu <mathieu@barata.fr>
2017-01-23 16:08:44 +0100

    debug BDD

--- application/controllers/praticien.php
+++ application/controllers/praticien.php
@@ -6,13 +6,14 @@ class Praticien extends CI_Controller {
 	//CONSTRUCTEUR PAR DEFAUT DE LA CLASSE Praticien
 
 	public function __construct(){
+            
 		//on appel le constructeur de la classe m�re
 		parent:: __construct();
 		
 	}
 	
 	public function index() {
-
+                
 		$this->load->model('Modele');
 		$data['query'] = $this->Modele->get_praticien();
 		$this->load->view('V_Praticiens.php', $data);
--- application/controllers/welcome.php
+++ application/controllers/welcome.php
@@ -1,20 +1,21 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-require('praticien.php'); //call class Praticien
 class Welcome extends CI_Controller {
 
 	/**
          * CONSTRUCTEUR PAR DEFAUT DE LA CLASSE Praticien
          */
 	public function __construct(){
+                
 		//on appel le constructeur de la classe m�re
 		parent:: __construct();
+                
 	}
 	/**
          * Cette méthode appelle la méthode du index du controller Praticien
          */
-	public function index() {
-            $practicien  = new Praticien();
-            $practicien->index();
+	public function index(){ 
+            
+            redirect('/praticien/index', 'refresh');
         }
 }
 
